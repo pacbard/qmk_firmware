78784681b29673b87db75e328fbfeaf61544b466
Emanuele Bardelli <bardellie@gmail.com>
2021-01-18 08:28:40 -0500

    Remove control selector from slash key\n\nI use capslock as control, so no need to have it also under slash

--- keyboards/kbdfans/kbd75/keymaps/eb/keymap.c
+++ keyboards/kbdfans/kbd75/keymaps/eb/keymap.c
@@ -10,7 +10,6 @@
 #define MODS_CTRL_MASK  (MOD_BIT(KC_LSHIFT)|MOD_BIT(KC_RSHIFT))
 #define ESCNAV LT(_NAV,KC_ESC)
 #define ESCHPR MT(MOD_HYPR,KC_ESC)
-#define CTLSLSH MT(MOD_LCTL,KC_SLSH)
 #define ___ KC_TRNS
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -46,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	   KC_LCTL   , KC_A  , KC_S  , KC_D  , KC_F  , KC_G  , KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,      KC_ENT     , KC_END,
 // |-------------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-----------------+-------|
 
-	  KC_LSFT ,KC_NO, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  , KC_N  , KC_M  ,KC_COMM, KC_DOT,CTLSLSH,    KC_RSFT   , KC_UP , KC_DEL,
+	  KC_LSFT ,KC_NO, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  , KC_N  , KC_M  ,KC_COMM, KC_DOT, KC_SLSH ,   KC_RSFT  , KC_UP , KC_DEL,
 //|-----------------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+--------------+-------+-------|
 
 	KC_LCTL  , KC_LALT, KC_LGUI,            KC_SPC, KC_SPC, KC_SPC                 ,MO(_FN),MO(_FN),KC_RALT,KC_LEFT,KC_DOWN,KC_RGHT
